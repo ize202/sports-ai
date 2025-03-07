6f9d6a7932d1e6903fb7f6b432145f3c1e251095
Aize Igbinakenzua <aizeakenzua@gmail.com>
2025-03-07 17:52:48 +0100

    Add dynamic screen height utility for mobile browsers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,9 @@ export default {
   ],
   theme: {
   	extend: {
+  		height: {
+  			'screen-dynamic': '100dvh',
+  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
